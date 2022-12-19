77ac1dd606354b9d4757fe1dd55d4fd6a10a3f48
Yegor S <syegorv@gmail.com>
2022-12-19 07:07:09 +0700

    add .

---
+++ src/main/java/web/config/JavaConfig.java
@@ -0,0 +1,71 @@
+package web.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@PropertySource({"classpath:db.properties"})
+@EnableTransactionManagement
+@ComponentScan(value = "java")
+public class JavaConfig {
+
+    @Resource
+    private final Environment env;
+
+    @Autowired
+    public JavaConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getRequiredProperty("db.driver"));
+        dataSource.setUsername(env.getRequiredProperty("db.username"));
+        dataSource.setPassword(env.getRequiredProperty("db.password"));
+        dataSource.setUrl(env.getRequiredProperty("db.url"));
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManager
+                = new LocalContainerEntityManagerFactoryBean();
+        entityManager.setDataSource(getDataSource());
+        entityManager.setPackagesToScan(env.getRequiredProperty("db.entity.package"));
+        entityManager.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManager.setJpaProperties(additionalProperties());
+        return entityManager;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        transactionManager.setDataSource(getDataSource());
+        return transactionManager;
+    }
+
+    Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        properties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        properties.put("hibernate.connection.characterEncoding", "utf8");
+        properties.put("hibernate.connection.CharSet", "utf8");
+        properties.put("hibernate.connection.useUnicode", true);
+        return properties;
+    }
+}
---
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -0,0 +1,44 @@
+package web.dao;
+
+import web.model.User;
+import org.springframework.stereotype.Repository;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public User getById(long id) {
+        return  entityManager.find(User.class, id);
+    }
+
+    @Override
+    public void addUser(User newUser) {
+        entityManager.persist(newUser);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        User user = entityManager.find(User.class, id);
+        entityManager.remove(user);
+    }
+
+    @Override
+    public void updateUser(long id, User userForUpdate) {
+        User toUpdate = getById(id);
+        toUpdate = entityManager.merge(userForUpdate);
+    }
+
+    @Override
+    public List<User> getUsersList() {
+        TypedQuery<User> query = entityManager
+                .createQuery("SELECT u FROM User u", User.class);
+        return query.getResultList();
+    }
+}
